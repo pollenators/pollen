702822b0c8c26107c9c6b8531c11192e4d18a554
pollenators <66385035+pollenators@users.noreply.github.com>
2020-06-08 15:16:42 -0400

    [Delete] Remove database files

--- init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE story (
-	ID SERIAL PRIMARY KEY,
-	author VARCHAR(300) NOT NULL,
-	title VARCHAR(300) NOT NULL
-);
-
-INSERT INTO story (author, title)
-VALUES  ('Jenn', 'Harry Potter');
