79f9a573ff437b88025fcde997bcb9833bae9e69
jenntang1 <1039@holbertonschool.com>
2020-06-10 17:24:51 +0000

    [Update] Change path to index.html

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.json())
+const express = require('express');
+const path = require('path');
+const app = express();
 
 
+app.use(express.static(__dirname + './svg'));
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + 'index.html'));
+});
 
-app.get('/', function (req, res) {
-  res.sendFile("index.html", { root: __dirname })
-})
-
-app.listen(port)
+app.listen(process.env.PORT || 3000);
