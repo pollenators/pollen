561c51b815bd039a4bba68005a9c42e0e58003b1
jenntang1 <1039@holbertonschool.com>
2020-06-15 21:52:43 +0000

    [Update] Add quiz

---
+++ public/grade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>pollen</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<header>
+			<nav class="navbar navbar-inverse">
+				<div class="container-fluid">
+				  <div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					  <span class="icon-bar"></span>
+					  <span class="icon-bar"></span>
+					  <span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="/public/index.html">POLLEN</a>
+				  </div>
+				  <div class="collapse navbar-collapse" id="myNavbar">
+					<ul class="nav navbar-nav">
+
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li class="active"><a href="/public/index.html">MAIN</a></li>
+						<li><a href="/public/about.html">ABOUT</a></li>
+						<li><a href="/public/quiz.php">QUIZ</a></li>  
+					</ul>
+				  </div>
+				</div>
+			  </nav>
+			  <h1>POLLEN</h1>
+		</header>
+		
+		<section class="animation">
+			<div class="story">
+				<div id="page-wrap">
+		
+        <?php
+            
+            $answer1 = $_POST['question-1-answers'];
+            $answer2 = $_POST['question-2-answers'];
+            $answer3 = $_POST['question-3-answers'];
+            $answer4 = $_POST['question-4-answers'];
+            $answer5 = $_POST['question-5-answers'];
+        
+            $totalCorrect = 0;
+            
+            if ($answer1 == "A") { $totalCorrect++; }
+            if ($answer2 == "C") { $totalCorrect++; }
+            if ($answer3 == "B") { $totalCorrect++; }
+            if ($answer4 == "A") { $totalCorrect++; }
+            if ($answer5) { $totalCorrect++; }
+            
+            echo "<div id='results'>$totalCorrect / 5 correct</div>";
+            
+        ?>
+	
+	</div>
+</body>
+</html>
