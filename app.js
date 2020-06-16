f051291dfb373568c3ffa9aa0efbe43ff0488903
pollenators <66385035+pollenators@users.noreply.github.com>
2020-06-16 11:19:56 -0400

    [Update] Fixing deployment

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('public'));
-}
+app.use(express.static('public'));
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'public'));
+app.get('/about.html', (request, response) => {
+	response.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(process.env.PORT || 3000);
