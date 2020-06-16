20a748ea51239da9544a24d23c6031747c8bb2c7
pollenators <66385035+pollenators@users.noreply.github.com>
2020-06-16 11:38:00 -0400

    [Update] Fixing deployment

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const router = express.Router();
 
-app.use(express.static('public'));
-
-app.get('/public/about.html', function (req, res) {
-	res.sendFile(path.join(__dirname + 'public/about.html'));
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+router.get('/challenge',function(req,res){
+  res.sendFile(path.join(__dirname+'/challenge.html'));
+});
+
+app.use('/', router);
+
+app.listen(process.env.port || 3000);
