43439d3afcb0b4fb7655314835cb803beda42c17
jenntang1 <1039@holbertonschool.com>
2020-06-16 18:40:14 +0000

    [Update] Fix routes

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
-const phpExpress = require('php-express')({
-  binPath: 'php'
+const phpExpress = require('../')({
+  binPath: '/usr/bin/php'
 });
 
 router.get('/',function(req,res){
@@ -19,14 +19,16 @@ router.get('/challenge',function(req,res){
   res.sendFile(path.join(__dirname+'/public/challenge.php'));
 });
 
-app.set('views', './public'));
+app.set('views', path.join(__dirname, 'views'));
 app.engine('php', phpExpress.engine);
 app.set('view engine', 'php');
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.all(/.+\.php$/, phpExpress.router);
 
 //app.use(express.static('public'));
-
+app.use(express.bodyParser());
 app.use('/', router);
 
 app.listen(process.env.PORT || 3000);
