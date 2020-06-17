c863ce1d4f3a1d5345d22bd762538eb817ab03c3
jenntang1 <1039@holbertonschool.com>
2020-06-17 16:07:10 -0400

    [Update] Fixing deployment

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+//const path = require('path');
 
-const router = express.Router();
-const phpExpress = require('php-express')({
-  binPath: 'php'
-});
+//const router = express.Router();
+//const phpExpress = require('php-express')({
+//  binPath: 'php'
+//});
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 3000));
+//app.set('port', (process.env.PORT || 3000));
 
-app.use('/', express.static(__dirname));
+//app.use('/', express.static(__dirname));
 
-app.set('views', path.join(__dirname, '/views'));
-app.engine('php', phpExpress.engine);
-app.set('view engine', 'php');
+//app.set('views', path.join(__dirname, '/views'));
+//app.engine('php', phpExpress.engine);
+//app.set('view engine', 'php');
 
-app.all(/.+\.php$/, phpExpress.router);
+//app.all(/.+\.php$/, phpExpress.router);
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/views/index.html'));
-});
+//router.get('/',function(req,res){
+//  res.sendFile(path.join(__dirname+'/views/index.html'));
+//});
 
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/views/about.html'));
-});
+//router.get('/about',function(req,res){
+//  res.sendFile(path.join(__dirname+'/views/about.html'));
+//});
 
-app.use('/', router);
+//app.use('/', router);
+
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000);
-
-//app.use(express.static('public'));
