f99b0be43dd789131d6fa5c8d74842adff7996ad
jenntang1 <1039@holbertonschool.com>
2020-06-17 22:49:04 -0400

    [Update] Fixing deployment

--- app.js
+++ app.js
@@ -1,24 +1,54 @@
-const path = require('path');
-const express = require('express');
-const app = express();
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
 
-const router = express.Router();
-const phpExpress = require('php-express')({
-  binPath: 'php'
-});
+    // Use php-express and config it
+    phpExpress = require('../')({
+        binPath: '/usr/bin/php'
+    });
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+// Init Express js
+var app = express();
+app.set('port', process.env.PORT || 3000);
 
-app.set('port', (process.env.PORT || 3000));
+// Body parser is required
+app.use(express.bodyParser());
 
-app.use('/', express.static(__dirname, '/views'));
-
-app.set('views', path.join(__dirname, '/views'));
+// Set view engine to php-express
+app.set('views', path.join(__dirname, 'views'));
 app.engine('php', phpExpress.engine);
 app.set('view engine', 'php');
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Routing all .php file to php-express
 app.all(/.+\.php$/, phpExpress.router);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Pollen app listening on port ' + app.get('port'));
+});
+
+//const path = require('path');
+//const express = require('express');
+//const app = express();
+
+//const router = express.Router();
+//const phpExpress = require('php-express')({
+//  binPath: 'php'
+//});
+
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.json());
+
+//app.set('port', (process.env.PORT || 3000));
+
+//app.use('/', express.static(__dirname, ));
+
+//app.set('views', path.join(__dirname, '/views'));
+//app.engine('php', phpExpress.engine);
+//app.set('view engine', 'php');
+
+//app.all(/.+\.php$/, phpExpress.router);
 
 //router.get('/',function(req,res){
 //  res.sendFile(path.join(__dirname + '/public/index.php'));
@@ -32,6 +62,6 @@ app.all(/.+\.php$/, phpExpress.router);
 
 //app.use(express.static('public'));
 
-app.listen(app.get('port'), function() {
-  console.log('Pollen app is running on port', app.get('port'));
-});
+//app.listen(app.get('port'), function() {
+//  console.log('Pollen app is running on port', app.get('port'));
+//});
