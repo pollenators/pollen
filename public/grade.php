dbcced1060ed0a8bd8522f45784a78409723f245
jenntang1 <1039@holbertonschool.com>
2020-06-18 11:43:07 -0400

    [Update] Fixing routes

--- public/grade.php
+++ public/grade.php
@@ -11,7 +11,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-		<script src="/views/animations.js"></script>
+		<script src="animations.js"></script>
 	</head>
 	<body>
 		<header>
